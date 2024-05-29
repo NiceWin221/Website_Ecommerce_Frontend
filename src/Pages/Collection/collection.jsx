2996129275ab4c6727313a97403275bef13bde18
NiceWin <saputrawindybintan@gmail.com>
2024-05-29 21:50:00 +0700

    responsive2

--- src/Pages/Collection/collection.jsx
+++ src/Pages/Collection/collection.jsx
@@ -16,6 +16,7 @@ import Shoes1s from './images/shoes1s.png'
 import Shoes2 from './images/shoes3.png'
 import Shoes2s from './images/shoes3s.png'
 import Background from './images/background.jpg'
+import PotoLG from './images/poto-lg.jpg'
 
 export default function Collection() {
   const inViewElement1 = useRef(null)
@@ -36,6 +37,7 @@ export default function Collection() {
   const shoesZoom = useRef(null)
   const [quantity, setQuantity] = useState(1)
   const [activeQuantity, setActiveQuantity] = useState(null)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
 
   const handleQuantityPlus = () => {
@@ -76,15 +78,28 @@ export default function Collection() {
       }, 300)
     }
 
-    if (inViewShoesWidth < viewport - 450) {
-      shoesZoom.current.style.transition = "transform 7s ease-in-out"
-      shoesWidth.current.style.width = "500px"
-      shoesZoom.current.style.transform = "scale(1.1)"
+    if (isMobile) {
+      if (inViewShoesWidth < viewport - 450) {
+        shoesZoom.current.style.transition = "transform 7s ease-in-out"
+        shoesWidth.current.style.width = "500px"
+        shoesZoom.current.style.transform = "scale(1.1)"
+      } else {
+        shoesWidth.current.style.width = "450px"
+        shoesZoom.current.style.transform = "scale(1)"
+        shoesZoom.current.style.transition = "transform 2s ease-in-out"
+      }
     } else {
-      shoesWidth.current.style.width = "450px"
-      shoesZoom.current.style.transform = "scale(1)"
-      shoesZoom.current.style.transition = "transform 2s ease-in-out"
+      if (inViewShoesWidth < viewport - 450) {
+        shoesWidth.current.style.width = "100%"
+        shoesZoom.current.style.transition = "transform 7s ease-in-out"
+        shoesZoom.current.style.transform = "scale(1.1)"
+      } else {
+        shoesWidth.current.style.width = "90%"
+        shoesZoom.current.style.transform = "scale(1)"
+        shoesZoom.current.style.transition = "transform 2s ease-in-out"
+      }
     }
+
 
     if (inViewFollow < viewport - 200) {
       inFollowUs.style.transform = "scale(1)";
@@ -154,14 +169,14 @@ export default function Collection() {
   }, []);
 
   return (
-    <div className="w-full">
+    <div className="w-full ">
       <h1 className="text-3xl font-bold text-black text-center mb-4">COLLECTION LIST</h1>
       <div className='display-parent'>
         <div className={`fadein relative bg-gray-500 w-full h-full lg:w-1/3 overflow-hidden`} ref={inViewElement1}>
           <img src={Poto1} alt="Shoes" className='opacity-60' />
-          <span className="absolute left-5 bottom-7">
-            <h1 className="text-base font-bold text-white mb-3">SHOES</h1>
-            <span className='collection-btn group'>
+          <span className="absolute left-5 bottom-7 lg:left-10 lg:bottom-10">
+            <h1 className="text-base font-bold text-white mb-3 lg:text-xl lg:mb-6">SHOES</h1>
+            <span className='collection-btn group lg:py-3 lg:px-5'>
               <button className="text-balck relative z-10 group-hover:text-white transition duration-500 ease-in-out">SHOP NOW</button>
               <span className='collection-btn-bg'></span>
             </span>
@@ -169,9 +184,9 @@ export default function Collection() {
         </div>
         <div className="fadein relative bg-gray-500 w-full h-full lg:w-1/3 overflow-hidden" ref={inViewElement2}>
           <img src={Poto2} alt="Shoes" className='opacity-60' />
-          <span className="absolute left-5 bottom-7">
-            <h1 className="text-base font-bold text-white mb-3">HOODIE</h1>
-            <span className='collection-btn group'>
+          <span className="absolute left-5 bottom-7 lg:left-10 lg:bottom-10">
+            <h1 className="text-base font-bold text-white mb-3 lg:text-xl lg:mb-6">HOODIE</h1>
+            <span className='collection-btn group lg:py-3 lg:px-5'>
               <button className="text-balck relative z-10 group-hover:text-white transition duration-500 ease-in-out">SHOP NOW</button>
               <span className='collection-btn-bg'></span>
             </span>
@@ -179,9 +194,9 @@ export default function Collection() {
         </div>
         <div className="fadein relative bg-gray-500 w-full h-full lg:w-1/3 overflow-hidden" ref={inViewElement3}>
           <img src={Poto3} alt="Shoes" className='opacity-60' />
-          <span className="absolute left-5 bottom-7">
-            <h1 className="text-base font-bold text-white mb-3">SANDAL</h1>
-            <span className='collection-btn group'>
+          <span className="absolute left-5 bottom-7 lg:left-10 lg:bottom-10">
+            <h1 className="text-base font-bold text-white mb-3 lg:text-xl lg:mb-6">SANDAL</h1>
+            <span className='collection-btn group lg:py-3 lg:px-5'>
               <button className="text-balck relative z-10 group-hover:text-white transition duration-500 ease-in-out">SHOP NOW</button>
               <span className='collection-btn-bg'></span>
             </span>
@@ -189,9 +204,9 @@ export default function Collection() {
         </div>
         <div className="fadein relative bg-gray-500 w-full h-full lg:w-1/3 overflow-hidden" ref={inViewElement4}>
           <img src={Poto4} alt="Shoes" className='opacity-60' />
-          <span className="absolute left-5 bottom-7">
-            <h1 className="text-base font-bold text-white mb-3">SHOES</h1>
-            <span className='collection-btn group'>
+          <span className="absolute left-5 bottom-7 lg:left-10 lg:bottom-10">
+            <h1 className="text-base font-bold text-white mb-3 lg:text-xl lg:mb-6">SHOES</h1>
+            <span className='collection-btn group lg:py-3 lg:px-5'>
               <button className="text-balck relative z-10 group-hover:text-white transition duration-500 ease-in-out">SHOP NOW</button>
               <span className='collection-btn-bg'></span>
             </span>
@@ -199,9 +214,9 @@ export default function Collection() {
         </div>
         <div className="fadein relative bg-gray-500 w-full h-full lg:w-1/3 overflow-hidden" ref={inViewElement5}>
           <img src={Poto5} alt="Shoes" className='opacity-60' />
-          <span className="absolute left-5 bottom-7">
-            <h1 className="text-base font-bold text-white mb-3">HOODIE</h1>
-            <span className='collection-btn group'>
+          <span className="absolute left-5 bottom-7 lg:left-10 lg:bottom-10">
+            <h1 className="text-base font-bold text-white mb-3 lg:text-xl lg:mb-6">HOODIE</h1>
+            <span className='collection-btn group lg:py-3 lg:px-5'>
               <button className="text-balck relative z-10 group-hover:text-white transition duration-500 ease-in-out">SHOP NOW</button>
               <span className='collection-btn-bg'></span>
             </span>
@@ -209,9 +224,9 @@ export default function Collection() {
         </div>
         <div className="fadein relative bg-gray-500 w-full h-full lg:w-1/3 overflow-hidden" ref={inViewElement6}>
           <img src={Poto6} alt="Shoes" className='opacity-60' />
-          <span className="absolute left-5 bottom-7">
-            <h1 className="text-base font-bold text-white mb-3">SANDAL</h1>
-            <span className='collection-btn group'>
+          <span className="absolute left-5 bottom-7 lg:left-10 lg:bottom-10">
+            <h1 className="text-base font-bold text-white mb-3 lg:text-xl lg:mb-6">SANDAL</h1>
+            <span className='collection-btn group lg:py-3 lg:px-5'>
               <button className="text-balck relative z-10 group-hover:text-white transition duration-500 ease-in-out">SHOP NOW</button>
               <span className='collection-btn-bg'></span>
             </span>
@@ -219,82 +234,87 @@ export default function Collection() {
         </div>
       </div>
       {/*  */}
-      <div className="w-full px-4 pt-4 flex flex-wrap mb-20 h-full">
-        <span className='w-full px-10 relative overflow-hidden h-[600px] lg:w-1/2'>
-          <span className='w-full h-[5%] absolute left-0 top-[390px] -z-10 bg-gradient-to-b from-white to-gray-500'></span>
-          <span className='w-full h-[5%] absolute left-0 top-[415px] -z-10 bg-gradient-to-t from-white to-gray-500'></span>
-          <span className='w-full h-[66%] absolute left-0 top-0 -z-10 bg-gradient-to-t from-white to-gray-300'></span>
-          <img src={Shoes} alt="Shoes" className='-z-10 w-full flex-shrink-0 mt-8 ml-6 adidas' ref={adidasInView} />
-          <h1 className='text-2xl font-bold text-center mt-2'>ADIDAS</h1>
-        </span>
-        <span className='w-full h-full px-4 py-5 bg-[#e0e0e0] lg:w-1/2'>
-          <span className='collection-parent flex flex-wrap justify-center w-full h-full' ref={collection}>
-            <h1 className='text-3xl font-bold text-black lg:w-full lg:text-center lg:g-full'>FLY WITH AT - 693 SERIES</h1>
-            <span className='collection relative mt-3 group'>
-              <button className='text-base font-semibold text-white z-10 relative group-hover:text-black'>SEE COLLECTION</button>
-              <span className='group-hover:w-0'></span>
-            </span>
+      <div className='lg:flex lg:justify-center'>
+        <div className="w-full px-4 pt-4 flex flex-wrap mb-20 h-full lg:w-[90%] lg:h-[750px]">
+          <span className='w-full px-10 relative overflow-hidden h-[600px] lg:w-1/2 lg:h-full'>
+            <span className='w-full h-[5%] absolute left-0 top-[390px] -z-10 bg-gradient-to-b from-white to-gray-500'></span>
+            <span className='w-full h-[5%] absolute left-0 top-[415px] -z-10 bg-gradient-to-t from-white to-gray-500'></span>
+            <span className='w-full h-[66%] absolute left-0 top-0 -z-10 bg-gradient-to-t from-white to-gray-300'></span>
+            <img src={Shoes} alt="Shoes" className='-z-10 w-full flex-shrink-0 mt-8 ml-6 adidas' ref={adidasInView} />
+            <h1 className='text-2xl font-bold text-center mt-2'>ADIDAS</h1>
           </span>
-        </span>
-      </div>
-      {/*  */}
-      <div className='follow w-full h-[400px] relative overflow-hidden mb-10 bg-gray-500'>
-        <img src={Background} alt="background" ref={followUs} className='opacity-70' />
-        <div className='absolute left-7' ref={followUsObject}>
-          <h1 className='text-lg font-bold text-white mb-2'>MAKE IT ELEGANT AND DIFFERENT</h1>
-          <h2 className='text-3xl font-bold text-white'>KICK BACK TO SCHOOL</h2>
-          <button className='text-sm font-bold text-black bg-white px-6 py-3 w-[126px] mt-3'>SHOP NOW</button>
-          <button className='block text-sm font-bold text-white px-6 py-3 mt-3 border border-white-1 w-[126px]'>SHOP ALL</button>
-          <p className='text-xl font-semibold mt-3 ml-2'>FOLLOW US</p>
+          <span className='collection-parent flex flex-wrap justify-center w-full h-full px-4 py-5 bg-[#e0e0e0] lg:w-1/2 lg:h-full items-center' ref={collection}>
+            <h1 className='text-3xl font-bold text-black lg:w-full text-center lg:text-5xl lg:relative lg:top-[150px] mb-3'>FLY WITH AT - 693 SERIES</h1>
+            <div className='relative h-fit py-3 px-6 group border border-black lg:relative lg:bottom-[125px]'>
+              <button className='text-lg font-bold text-white relative z-10 group-hover:text-black transition-all duration-500 ease-in-out'>SEE COLLECTION</button>
+              <span className='absolute bg-black left-[50%] top-0 w-full h-full transform translate-x-[-50%] group-hover:w-0 transition-all duration-300 ease-in-out'></span>
+            </div>
+          </span>
         </div>
       </div>
       {/*  */}
-      <div className='collection-shoes w-full flex flex-wrap gap-5'>
-        <span className='display relative w-full group' ref={shoesDisplay1}>
-          <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
-          <img src={Shoes1} alt="Shoes" className='group-hover:opacity-0' />
-          <img src={Shoes1s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
-          <h1 className='font-bold text-base px-2'>TIMELESS LOW BLACK WHITE STZ</h1>
-          <div className='flex gap-3 px-2'>
-            <p>Rp 235.000,00</p>
-            <p className='text-slate-500 line-through'>Rp 345.000,00</p>
-          </div>
-        </span>
-        <span className='display relative w-full group' ref={shoesDisplay2}>
-          <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
-          <img src={Shoes2} alt="Shoes" className='group-hover:opacity-0' />
-          <img src={Shoes2s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
-          <h1 className='font-bold text-base px-2'>TIMELESS LOW BLACK GUM STZ</h1>
-          <div className='flex gap-3 px-2'>
-            <p>Rp 235.000,00</p>
-            <p className='text-slate-500 line-through'>Rp 345.000,00</p>
-          </div>
-        </span>
-        <span className='display relative w-full group' ref={sandalDisplay1}>
-          <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
-          <img src={Sandal1} alt="Shoes" className='group-hover:opacity-0' />
-          <img src={Sandal1s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
-          <h1 className='font-bold text-base px-2'>CLASSIC SOLID GREY BLACK 2</h1>
-          <div className='flex gap-3 px-2'>
-            <p>Rp 50.000,00</p>
-            <p className='text-slate-500 line-through'>Rp 125.000,00</p>
-          </div>
-        </span>
-        <span className='display relative w-full group' ref={sandalDisplay2}>
-          <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
-          <img src={Sandal2} alt="Shoes" className='group-hover:opacity-0' />
-          <img src={Sandal2s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
-          <h1 className='font-bold text-base px-2'>CLASSIC SOLID BLACK ORANGE 2</h1>
-          <div className='flex gap-3 px-2'>
-            <p>Rp 50.000,00</p>
-            <p className='text-slate-500 line-through'>Rp 125.000,00</p>
-          </div>
-        </span>
+      <div className='follow w-full h-[400px] relative overflow-hidden mb-10 bg-gray-500 lg:h-[100vh]'>
+        <img src={Background} alt="background" ref={followUs} className='opacity-70 lg:w-full lg:relative lg:bottom-[300px]' />
+        <div className='absolute left-7 lg:left-[100px] lg:mb-16' ref={followUsObject}>
+          <h1 className='text-lg font-bold text-white mb-2 lg:text-xl'>MAKE IT ELEGANT AND DIFFERENT</h1>
+          <h2 className='text-3xl font-bold text-white lg:text-5xl'>KICK BACK TO SCHOOL</h2>
+          <span className='lg:flex lg:gap-7 lg:mt-4'>
+            <button className='text-sm font-bold text-black bg-white px-6 py-3 w-[126px] mt-3'>SHOP NOW</button>
+            <button className='block text-sm font-bold text-white px-6 py-3 mt-3 border border-white-1 w-[126px]'>SHOP ALL</button>
+          </span>
+          <p className='text-xl font-semibold mt-3 ml-2 lg:font-bold'>FOLLOW US</p>
+        </div>
       </div>
       {/*  */}
-      <div className='w-full flex justify-center mt-20 relative'>
-        <div className='shoes-width w-[450px] relative overflow-hidden h-[500px] flex flex-col justify-end' ref={shoesWidth}>
-          <img src={Poto1} alt="Shoes" className='w-full h-full object-cover absolute' ref={shoesZoom} />
+      <div className='lg:flex lg:justify-center'>
+        <div className='collection-shoes w-full flex flex-wrap gap-5 lg:w-[90%] lg:gap-0'>
+          <span className='display relative w-full group lg:w-[25%]' ref={shoesDisplay1}>
+            <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
+            <img src={Shoes1} alt="Shoes" className='group-hover:opacity-0' />
+            <img src={Shoes1s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
+            <h1 className='font-bold text-base px-2'>TIMELESS LOW BLACK WHITE STZ</h1>
+            <div className='flex gap-3 px-2'>
+              <p>Rp 235.000,00</p>
+              <p className='text-slate-500 line-through'>Rp 345.000,00</p>
+            </div>
+          </span>
+          <span className='display relative w-full group lg:w-[25%]' ref={shoesDisplay2}>
+            <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
+            <img src={Shoes2} alt="Shoes" className='group-hover:opacity-0' />
+            <img src={Shoes2s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
+            <h1 className='font-bold text-base px-2'>TIMELESS LOW BLACK GUM STZ</h1>
+            <div className='flex gap-3 px-2'>
+              <p>Rp 235.000,00</p>
+              <p className='text-slate-500 line-through'>Rp 345.000,00</p>
+            </div>
+          </span>
+          <span className='display relative w-full group lg:w-[25%]' ref={sandalDisplay1}>
+            <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
+            <img src={Sandal1} alt="Shoes" className='group-hover:opacity-0' />
+            <img src={Sandal1s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
+            <h1 className='font-bold text-base px-2'>CLASSIC SOLID GREY BLACK 2</h1>
+            <div className='flex gap-3 px-2'>
+              <p>Rp 50.000,00</p>
+              <p className='text-slate-500 line-through'>Rp 125.000,00</p>
+            </div>
+          </span>
+          <span className='display relative w-full group lg:w-[25%]' ref={sandalDisplay2}>
+            <h1 className='text-sm text-white bg-black py-2 px-4 font-bold w-fit'>SALE</h1>
+            <img src={Sandal2} alt="Shoes" className='group-hover:opacity-0' />
+            <img src={Sandal2s} alt="Shoes" className='absolute left-0 top-0 hidden group-hover:block' />
+            <h1 className='font-bold text-base px-2'>CLASSIC SOLID BLACK ORANGE 2</h1>
+            <div className='flex gap-3 px-2'>
+              <p>Rp 50.000,00</p>
+              <p className='text-slate-500 line-through'>Rp 125.000,00</p>
+            </div>
+          </span>
+        </div>
+      </div>
+      {/*  */}
+      <div className='w-full flex justify-center mt-20 lg:w-full'>
+        <div className='w-[450px] overflow-hidden relative h-[500px] flex flex-col justify-end lg:h-[100vh] lg:w-[90%] transition-all duration-500 ease-in-out' ref={shoesWidth}>
+          <img src={PotoLG} alt="Shoes" className='hidden lg:block object-cover w-full h-full absolute' ref={shoesZoom} />
+          <img src={Poto1} alt="Shoes" className='w-full h-full object-cover absolute lg:hidden' ref={shoesZoom} />
           <span className='flex items-center gap-2 text-white font-semibold justify-center text-lg relative z-10 mb-8'>
             <a href="https://www.instagram.com/wybnsa/" target="_blank">
               <i className="fa-brands fa-instagram text-slate-500 cursor-pointer hover:text-slate-200 transition duration-300 ease-out"></i>
@@ -313,7 +333,6 @@ export default function Collection() {
       </div>
       {/*  */}
       <div className='w-full text-center relative mb-20 bg-slate-200'>
-=======
       <div className='w-full text-center relative mb-20'>
       <h1 className='text-xl font-semibold text-black'>GAVIN STANCE WHITE GUM</h1>
       <h2 className='text-3xl font-bold text-black'>GAVIN STANCE WHITE GUM</h2>
